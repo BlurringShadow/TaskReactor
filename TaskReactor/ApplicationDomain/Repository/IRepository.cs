85eb272c4276f0282d75446b98cf01fd88bd585c
Blurring Shadow <976999484@qq.com>
2020-05-30 21:03:40 +0800

    Repository: Add conment Extract IRepository interface

---
+++ TaskReactor/ApplicationDomain/Repository/IRepository.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ApplicationDomain.Models.Database.Entity;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationDomain.Repository
+{
+    internal interface IRepository<TDataBaseModel, out TDbContext>
+        where TDataBaseModel : DatabaseModel where TDbContext : DbContext
+
+    {
+        /// <summary>
+        /// Database context <see cref="DbContext"/>
+        /// </summary>
+        [NotNull] DbSet<TDataBaseModel> DbSet { get; }
+
+        /// <summary>
+        /// Entity set <see cref="DbSet{T}"/>
+        /// </summary>
+        [NotNull] TDbContext Context { get; }
+
+        /// <summary>
+        /// Find if table contains the key.
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <returns> Return an async task and task.value is true if contains. </returns>
+        [NotNull]
+        Task<bool> ContainsByKeyAsync(IEnumerable keys);
+
+        /// <summary>
+        /// Find if table contains the key
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Return an async task and task.value is true if contains. </returns>
+        [NotNull]
+        Task<bool> ContainsByKeyAsync(IEnumerable keys, CancellationToken token);
+
+        /// <summary>
+        /// Find with keys
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <returns> Return an async task with finding result. </returns>
+        ValueTask<TDataBaseModel> FindByKeys(IEnumerable keys);
+
+        /// <summary>
+        /// Find with keys
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Return an async task with finding result. </returns>
+        ValueTask<TDataBaseModel> FindByKeys(IEnumerable keys, CancellationToken token);
+
+        /// <summary>
+        /// Remove the entities
+        /// </summary>
+        void Remove([NotNull] IEnumerable<TDataBaseModel> models);
+
+        /// <summary>
+        /// Remove the entities
+        /// </summary>
+        void Remove([NotNull] params TDataBaseModel[] models);
+
+        /// <summary>
+        /// Remove all the entities
+        /// </summary>
+        /// <returns> Async running task </returns>
+        [NotNull]
+        Task RemoveAllAsync();
+
+        /// <summary>
+        /// Update the entities
+        /// </summary>
+        void Update([NotNull] params TDataBaseModel[] models);
+
+        /// <summary>
+        /// Update the entities
+        /// </summary>
+        void Update([NotNull] IEnumerable<TDataBaseModel> models);
+
+
+        /// <summary>
+        /// Sync changes into database
+        /// </summary>
+        /// <returns> Async task with affected rows </returns>
+        [NotNull]
+        Task<int> DbSync();
+
+
+        /// <summary>
+        /// Sync changes into database
+        /// </summary>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Async task with affected rows </returns>
+        [NotNull]
+        Task<int> DbSync(CancellationToken token);
+    }
+}
--- TaskReactor/ApplicationDomain/Repository/Repository.cs
+++ TaskReactor/ApplicationDomain/Repository/Repository.cs
@@ -9,18 +9,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationDomain.Repository
 {
-    public abstract class Repository<TDataBaseModel, TDbContext> where TDbContext : DbContext
+    public abstract class Repository<TDataBaseModel, TDbContext> : IRepository<TDataBaseModel, TDbContext>
+        where TDbContext : DbContext
         where TDataBaseModel : DatabaseModel
     {
-        [NotNull] public readonly TDbContext Context;
+        public TDbContext Context { get; }
 
-        [NotNull] public DbSet<TDataBaseModel> DbSet => Context.Set<TDataBaseModel>()!;
+        public DbSet<TDataBaseModel> DbSet => Context.Set<TDataBaseModel>()!;
 
         protected Repository([NotNull] TDbContext context) => Context = context;
 
-        public async Task<bool> ContainsByKey(IEnumerable keys) => await ContainsByKey(keys, CancellationToken.None);
+        public async Task<bool> ContainsByKeyAsync(IEnumerable keys) => await ContainsByKeyAsync(keys, CancellationToken.None);
 
-        public async Task<bool> ContainsByKey(IEnumerable keys, CancellationToken token) =>
+        public async Task<bool> ContainsByKeyAsync(IEnumerable keys, CancellationToken token) =>
             !(await FindByKeys(keys, token) is null);
 
         public async ValueTask<TDataBaseModel> FindByKeys(IEnumerable keys) =>
@@ -29,21 +30,19 @@ namespace ApplicationDomain.Repository
         public async ValueTask<TDataBaseModel> FindByKeys(IEnumerable keys, CancellationToken token) =>
             (await DbSet.FindAsync(keys, token))!;
 
-        public void Remove([NotNull] params TDataBaseModel[] models) => Remove((IEnumerable<TDataBaseModel>)models);
+        public void Remove(params TDataBaseModel[] models) => Remove((IEnumerable<TDataBaseModel>)models);
 
-        public void Remove([NotNull] IEnumerable<TDataBaseModel> models) => DbSet.RemoveRange(models);
+        public void Remove(IEnumerable<TDataBaseModel> models) => DbSet.RemoveRange(models);
 
-        public async Task RemoveAll() => await Context.DeleteTableFromDbSetAsync<TDataBaseModel>();
+        public async Task RemoveAllAsync() => await Context.DeleteTableFromDbSetAsync<TDataBaseModel>();
 
-        public void Update([NotNull] params TDataBaseModel[] models) => Update((IEnumerable<TDataBaseModel>)models);
+        public void Update(params TDataBaseModel[] models) => Update((IEnumerable<TDataBaseModel>)models);
 
-        public void Update([NotNull] IEnumerable<TDataBaseModel> models) => DbSet.UpdateRange(models);
+        public void Update(IEnumerable<TDataBaseModel> models) => DbSet.UpdateRange(models);
 
-        [NotNull]
         public async Task<int> DbSync() => await DbSync(CancellationToken.None);
 
-        [NotNull]
-        public async Task<int> DbSync(CancellationToken cancellationToken) =>
-            await Context.SaveChangesAsync(cancellationToken)!;
+        public async Task<int> DbSync(CancellationToken token) =>
+            await Context.SaveChangesAsync(token)!;
     }
 }
