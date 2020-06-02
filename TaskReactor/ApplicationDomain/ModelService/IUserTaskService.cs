3cb2ebcd79d354bdb3adc668f6f6e61f08fac8c5
Blurring Shadow <976999484@qq.com>
2020-06-02 14:03:19 +0800

    Add user task service interface and implmentation.

---
+++ TaskReactor/ApplicationDomain/ModelService/IUserTaskService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using ApplicationDomain.Database;
+using ApplicationDomain.Database.Entity;
+using ApplicationDomain.DataModel;
+using ApplicationDomain.DataRepository;
+using JetBrains.Annotations;
+
+namespace ApplicationDomain.ModelService
+{
+    [InheritedExport]
+    public interface IUserTaskService : IService<UserTask, TaskReactorDbContext, IUserTaskRepository, UserTaskModel>
+    {
+        /// <summary>
+        /// Get all tasks from user
+        /// </summary>
+        /// <param name="user"> The user </param>
+        /// <returns> Async result contains list of <see cref="UserTask"/> </returns>
+        [NotNull]
+        Task<List<UserTask>> GetAllFromUserAsync([NotNull] UserModel user);
+
+        /// <summary>
+        /// Get all tasks from user
+        /// </summary>
+        /// <param name="user"> The user </param>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Async result contains list of <see cref="UserTask"/> </returns>
+        [NotNull]
+        Task<List<UserTask>> GetAllFromUserAsync([NotNull] UserModel user, CancellationToken token);
+    }
+}
---
+++ TaskReactor/ApplicationDomain/ModelService/UserTaskService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using ApplicationDomain.Database;
+using ApplicationDomain.Database.Entity;
+using ApplicationDomain.DataModel;
+using ApplicationDomain.DataRepository;
+using JetBrains.Annotations;
+
+namespace ApplicationDomain.ModelService
+{
+    sealed class UserTaskService : Service<UserTask, TaskReactorDbContext, IUserTaskRepository, UserTaskModel>,
+        IUserTaskService
+    {
+        [ImportingConstructor]
+        public UserTaskService([NotNull] IUserTaskRepository repository) : base(repository)
+        {
+        }
+
+        /// <summary>
+        /// Get all tasks from user
+        /// </summary>
+        /// <param name="user"> The user </param>
+        /// <returns> Async result contains list of <see cref="UserTask"/> </returns>
+        public async Task<List<UserTask>> GetAllFromUserAsync(UserModel user) =>
+            await GetAllFromUserAsync(user, CancellationToken.None);
+
+        /// <summary>
+        /// Get all tasks from user
+        /// </summary>
+        /// <param name="user"> The user </param>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Async result contains list of <see cref="UserTask"/> </returns>
+        public async Task<List<UserTask>> GetAllFromUserAsync(UserModel user, CancellationToken token) =>
+            await Repository.GetAllFromUserAsync(user._dataBaseModel, token);
+    }
+}
