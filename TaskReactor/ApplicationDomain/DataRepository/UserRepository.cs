252f7b3119b531462ac3bc15026f6be0575a9e7d
Blurring Shadow <976999484@qq.com>
2020-06-02 12:54:06 +0800

    Extract user repository interface, change UserRepository's access modifier to internal.

--- TaskReactor/ApplicationDomain/DataRepository/UserRepository.cs
+++ TaskReactor/ApplicationDomain/DataRepository/UserRepository.cs
@@ -7,28 +7,23 @@ using JetBrains.Annotations;
 
 namespace ApplicationDomain.DataRepository
 {
-    [Export]
-    public sealed class UserRepository : Repository<User, TaskReactorDbContext>
+    sealed class UserRepository : Repository<User, TaskReactorDbContext>, IUserRepository
     {
         [ImportingConstructor]
         public UserRepository([NotNull] TaskReactorDbContext dbContext) : base(dbContext)
         {
         }
 
-        /// <summary>
-        /// Register a uer
-        /// </summary>
-        /// <param name="user"> Id will be reassigned after finishing update </param>
-        public void Register([NotNull] User user) => Update(user);
+        public void Register(User user) => Update(user);
 
-        public async Task<User> LogInAsync([NotNull] User user) => await LogInAsync(user, CancellationToken.None);
+        public async Task<User> LogInAsync(User user) => await LogInAsync(user, CancellationToken.None);
 
-        public async Task<User> LogInAsync([NotNull] User user, CancellationToken token)
+        public async Task<User> LogInAsync(User user, CancellationToken token)
         {
             var found = await DbSet.FindAsync(new object[] {user.Id}, token);
             return found?.Password == user.Password ? found : null;
         }
 
-        public void LogOff([NotNull] User user) => Remove(user);
+        public void LogOff(User user) => Remove(user);
     }
 }
