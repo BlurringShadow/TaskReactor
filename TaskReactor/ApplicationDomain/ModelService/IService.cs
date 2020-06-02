edfd614f17f16f9e6555ae4a21c02fdd6b372339
Blurring Shadow <976999484@qq.com>
2020-06-02 11:52:36 +0800

    Complete service abstract api

---
+++ TaskReactor/ApplicationDomain/ModelService/IService.cs
@@ -0,0 +1,98 @@
+﻿using ApplicationDomain.Database.Entity;
+using ApplicationDomain.DataModel;
+using ApplicationDomain.DataRepository;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationDomain.ModelService
+{
+    public interface IService<TDatabaseModel, out TDbContext, out TRepository, TModel>
+        where TDatabaseModel : DatabaseModel, new()
+        where TDbContext : DbContext
+        where TRepository : IRepository<TDatabaseModel, TDbContext>
+        where TModel : Model<TDatabaseModel>, new()
+    {
+        /// <summary>
+        /// <see cref="IRepository{T, U}"/>
+        /// </summary>
+        [NotNull] TRepository Repository { get; }
+
+        /// <summary>
+        /// Find if table contains the key. Input <paramref name="keys"/> should match the entity key type order
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <returns> Return an async task and task.value is true if contains. </returns>
+        [NotNull]
+        Task<bool> ContainsByKeyAsync([NotNull] IEnumerable keys);
+
+        /// <summary>
+        /// Find if table contains the key. Input <paramref name="keys"/> should match the entity key type order
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Return an async task and task.value is true if contains. </returns>
+        [NotNull]
+        Task<bool> ContainsByKeyAsync([NotNull] IEnumerable keys, CancellationToken token);
+
+
+        /// <summary>
+        /// Find if table contains the key. Input <paramref name="keys"/> should match the entity key type order
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <returns> Return an async task with finding result. </returns>
+        ValueTask<TModel> FindByKeysAsync([NotNull] IEnumerable keys);
+
+        /// <summary>
+        /// Find if table contains the key. Input <paramref name="keys"/> should match the entity key type order
+        /// </summary>
+        /// <param name="keys"> input keys </param>
+        /// <param name="token"> <see cref="CancellationToken"/> </param>
+        /// <returns> Return an async task with finding result. </returns>
+        ValueTask<TModel> FindByKeysAsync([NotNull] IEnumerable keys, CancellationToken token);
+
+        /// <summary>
+        /// Remove the models in database
+        /// </summary>
+        void Remove([ItemNotNull, NotNull] IEnumerable<TModel> models);
+
+        /// <summary>
+        /// Remove the models in database
+        /// </summary>
+        void Remove([ItemNotNull, NotNull] params TModel[] models);
+
+        /// <summary>
+        /// Remove all the models in database
+        /// </summary>
+        /// <returns> Async running task </returns>
+        [NotNull]
+        Task RemoveAllAsync();
+
+        /// <summary>
+        /// Update the models in database
+        /// </summary>
+        void Update([ItemNotNull, NotNull] IEnumerable<TModel> models);
+
+        /// <summary>
+        /// Update the models in database
+        /// </summary>
+        void Update([ItemNotNull, NotNull] params TModel[] models);
+
+        /// <summary>
+        /// Sync changes into database
+        /// </summary>
+        /// <returns> Async task with affected rows </returns>
+        [NotNull]
+        Task<int> DbSync();
+
+        /// <summary>
+        /// Sync changes into database
+        /// </summary>
+        /// <returns> Async task with affected rows </returns>
+        [NotNull]
+        Task<int> DbSync(CancellationToken token);
+    }
+}
--- TaskReactor/ApplicationDomain/ModelService/Service.cs
+++ TaskReactor/ApplicationDomain/ModelService/Service.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using ApplicationDomain.Database;
 using ApplicationDomain.Database.Entity;
 using ApplicationDomain.DataModel;
 using ApplicationDomain.DataRepository;
@@ -9,26 +12,42 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationDomain.ModelService
 {
-    public abstract class Service<TDatabaseModel, TDbContext, TRepository, TModel>
-        where TDatabaseModel : DatabaseModel, new()
+    public abstract class Service<TDatabaseModel, TDbContext, TRepository, TModel> : IService<TDatabaseModel, TDbContext, TRepository, TModel> where TDatabaseModel : DatabaseModel, new()
         where TDbContext : DbContext
         where TRepository : IRepository<TDatabaseModel, TDbContext>
-        where TModel : Model<TDatabaseModel>
+        where TModel : Model<TDatabaseModel>, new()
     {
-        [NotNull] public TRepository Repository { get; }
-
-        [NotNull] public TDbContext Context => Repository.Context;
-
-        [NotNull] public DbSet<TDatabaseModel> DbSet => Repository.DbSet;
+        public TRepository Repository { get; }
 
         protected Service([NotNull] TRepository repository) => Repository = repository;
 
-        [NotNull]
         public async Task<bool> ContainsByKeyAsync(IEnumerable keys) =>
             await ContainsByKeyAsync(keys, CancellationToken.None);
 
-        [NotNull]
         public async Task<bool> ContainsByKeyAsync(IEnumerable keys, CancellationToken token) =>
             await Repository.ContainsByKeyAsync(keys, token);
+
+        public async ValueTask<TModel> FindByKeysAsync(IEnumerable keys) =>
+            await FindByKeysAsync(keys, CancellationToken.None);
+
+        public async ValueTask<TModel> FindByKeysAsync(IEnumerable keys, CancellationToken token) =>
+            ModelConstructionProvider<TModel, TDatabaseModel>.CreateModelInstance(
+                await Repository.FindByKeysAsync(keys, token)
+            );
+
+        public void Remove(params TModel[] models) => Remove((IEnumerable<TModel>)models);
+
+        public void Remove(IEnumerable<TModel> models) =>
+            Repository.Remove(from model in models select model._dataBaseModel);
+
+        public async Task RemoveAllAsync() => await Repository.Context.DeleteTableFromDbSetAsync<TDatabaseModel>();
+
+        public void Update(params TModel[] models) => Update((IEnumerable<TModel>)models);
+
+        public void Update(IEnumerable<TModel> models) => Repository.Update(from model in models select model._dataBaseModel);
+
+        public async Task<int> DbSync() => await DbSync(CancellationToken.None);
+
+        public async Task<int> DbSync(CancellationToken token) => await Repository.DbSync(token)!;
     }
 }
