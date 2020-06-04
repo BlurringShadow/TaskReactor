2800b29765c4b620cb4872af17e99578166b2080
huobao98 <55397201+huobao98@users.noreply.github.com>
2020-06-04 22:47:12 +0800

    Add files via upload

---
+++ TaskReactor/Presentation/Views/LoginView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Presentation.Views
+{
+    /// <summary>
+    /// LoginView.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginView
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TaskReactor/Presentation/Views/RegisterView.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Presentation.Views
+{
+    /// <summary>
+    /// RegisterView.xaml 的交互逻辑
+    /// </summary>
+    public partial class RegisterView
+    {
+        public RegisterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
