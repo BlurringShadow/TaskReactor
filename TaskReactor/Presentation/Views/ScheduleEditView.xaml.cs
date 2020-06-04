148d98e0f3c4d42bc766c7a342822dd323165c38
Blurring Shadow <976999484@qq.com>
2020-06-04 23:16:57 +0800

    Reformat

--- TaskReactor/Presentation/Views/ScheduleEditView.xaml.cs
+++ TaskReactor/Presentation/Views/ScheduleEditView.xaml.cs
@@ -1,31 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Presentation.Views
+﻿namespace Presentation.Views
 {
     /// <summary>
-    /// ScheduleEdit.xaml 的交互逻辑
+    /// ScheduleEditView UI
     /// </summary>
-    public partial class ScheduleEditView : Page
+    public partial class ScheduleEditView
     {
-        public ScheduleEditView()
-        {
-            InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            throw new NotImplementedException();
-        }
+        public ScheduleEditView() => InitializeComponent();
     }
 }
