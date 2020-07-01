6fa326060a98e242c4dd69407e0a0f7e9800d0b1
yjmaa <55649083+yjmaa@users.noreply.github.com>
2020-07-01 20:25:12 +0800

    Update UsertaskNotificationView.xaml.cs

--- TaskReactor/Presentation/Views/UsertaskNotificationView.xaml.cs
+++ TaskReactor/Presentation/Views/UsertaskNotificationView.xaml.cs
@@ -1,26 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Notifications.Wpf.Core.Caliburn.Micro.Sample.Views
+namespace Presentation.Views
 {
-    /// <summary>
-    /// Interaction logic for NotificationView.xaml
-    /// </summary>
-    public partial class NotificationView : UserControl
+    public sealed partial class UsertaskNotificationView
     {
-        public NotificationView()
-        {
-            InitializeComponent();
-        }
+        public UsertaskNotificationView() => InitializeComponent();
     }
 }
