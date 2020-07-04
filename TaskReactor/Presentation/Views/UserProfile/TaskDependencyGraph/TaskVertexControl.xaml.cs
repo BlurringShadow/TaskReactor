a7daf89d1abc5e4fbe37f5b94aa1121ac153d270
Blurring Shadow <976999484@qq.com>
2020-07-04 23:12:31 +0800

    Add vertex data and control class

---
+++ TaskReactor/Presentation/Views/UserProfile/TaskDependencyGraph/TaskVertexControl.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Presentation.Views.UserProfile.TaskDependencyGraph
+{
+    public partial class TaskVertexControl
+    {
+        static TaskVertexControl() => DefaultStyleKeyProperty?.OverrideMetadata(
+            typeof(TaskVertexControl), new FrameworkPropertyMetadata(typeof(TaskVertexControl))
+        );
+
+        public TaskVertexControl(UserTaskVertex vertexData, bool tracePositionChange = false, bool bindToDataObject = true) :
+            base(vertexData, tracePositionChange, bindToDataObject)
+        {
+        }
+    }
+}
---
+++ TaskReactor/Presentation/Views/UserProfile/TaskDependencyGraph/UserTaskVertex.cs
@@ -0,0 +1,47 @@
+﻿#region File Info
+
+// Created by BlurringShadow,
+// Year: 2020
+// Month: 07
+// Day: 04
+// Time: 下午 10:17
+
+#endregion
+
+using System;
+using System.ComponentModel.Composition;
+using ApplicationDomain.DataModel;
+using ApplicationDomain.ModelService;
+using GraphX.Common.Enums;
+using GraphX.Common.Interfaces;
+using JetBrains.Annotations;
+
+namespace Presentation.Views.UserProfile.TaskDependencyGraph
+{
+    [Export, PartCreationPolicy(CreationPolicy.NonShared)]
+    public class UserTaskVertex : IGraphXVertex, IEquatable<UserTaskVertex>
+    {
+        [NotNull] readonly IUserTaskService _service;
+
+        [ImportingConstructor]
+        public UserTaskVertex([NotNull] IUserTaskService service) => _service = service;
+
+        public UserTaskModel Task { get; private set; }
+
+        public double Angle { get; set; }
+
+        public int GroupId { get; set; }
+
+        public ProcessingOptionEnum SkipProcessing { get; set; }
+
+        public long ID { get => Task?.Identity ?? 0; set => Task = _service.FindByKeysAsync(value).Result; }
+
+        public bool Equals(IGraphXVertex other) => other is UserTaskVertex vertex && Equals(vertex);
+
+        public bool Equals(UserTaskVertex other) => ID == other?.ID;
+
+        public override bool Equals(object obj) => obj is UserTaskVertex vertex && Equals(vertex);
+
+        public override int GetHashCode() => ID.GetHashCode();
+    }
+}
